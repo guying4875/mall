566486358f3f1a190420ebeec5740dbc102ef4c5
guying4875 <zhjx2015@aliyun.com>
2018-12-28 22:46:08 +0800

    commit

--- mall-web/src/main/java/com/mall/web/admin/controller/AdminController.java
+++ mall-web/src/main/java/com/mall/web/admin/controller/AdminController.java
@@ -1,6 +1,5 @@
 package com.mall.web.admin.controller;
 
-import com.alibaba.dubbo.config.annotation.Reference;
 import com.mall.web.admin.service.IAdminService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("admin")
 public class AdminController {
 
-    @Reference
-    IAdminService adminService;
+//    IAdminService adminService;
 
     @RequestMapping("hello")
     @ResponseBody
     public String hello(){
-        return adminService.hello();
+//        return adminService.hello();
+        return "222";
     }
 }
