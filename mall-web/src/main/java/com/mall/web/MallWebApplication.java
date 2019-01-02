7289bb6b7c7c05dcc7141d214d3c383b3eedc195
zhjx <zhjx2015@aliyun.com>
2019-01-02 22:25:54 +0800

    commit

--- mall-web/src/main/java/com/mall/web/MallWebApplication.java
+++ mall-web/src/main/java/com/mall/web/MallWebApplication.java
@@ -2,8 +2,10 @@ package com.mall.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("dubbo-config.xml")
 public class MallWebApplication {
 
     public static void main(String[] args) {
