7006493b6380ff7fcc986dc9a98acbc785d996ff
guying4875 <zhjx2015@aliyun.com>
2019-01-05 21:12:18 +0800

    dubbo

--- mall-ums/src/main/java/com/mall/ums/UmsApplication.java
+++ mall-ums/src/main/java/com/mall/ums/UmsApplication.java
@@ -1,11 +1,12 @@
 package com.mall.ums;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("dubbo-config.xml")
+@EnableDubboConfig
 public class UmsApplication {
 
     public static void main(String[] args) {
--- mall-web/src/main/java/com/mall/web/MallWebApplication.java
+++ mall-web/src/main/java/com/mall/web/MallWebApplication.java
@@ -1,11 +1,12 @@
 package com.mall.web;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("dubbo-config.xml")
+@EnableDubboConfig
 public class MallWebApplication {
 
     public static void main(String[] args) {
